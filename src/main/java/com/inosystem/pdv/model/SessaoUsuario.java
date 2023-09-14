22f69454c5e3b4a98d1258a9262a81c527b065c6
Jrpbjr <jrpbjr@gmail.com>
2023-09-14 13:42:06 -0300

    add model session user

---
+++ src/main/java/com/inosystem/pdv/model/SessaoUsuario.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inosystem.pdv.model;
+
+/**
+ *
+ * @author jrpbj
+ */
+public class SessaoUsuario {
+
+    public static Integer codigo;
+    public static String login;
+    public static String nome;
+    public static String ipServidor;
+    public static String nomeDoBanco;
+    public static String usuarioBanco;
+    public static String senhaBanco;
+    public static String caminhoMySQL;
+    public static String caminhoMySQLDump;
+    public static Integer quantidadeMesas;
+}
