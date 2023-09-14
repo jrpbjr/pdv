123e0974b4a044633ea908242ff5b28b6d148604
Jrpbjr <jrpbjr@gmail.com>
2023-09-14 13:38:41 -0300

    add model report produto

---
+++ src/main/java/com/inosystem/pdv/model/RelatorioProduto.java
@@ -0,0 +1,137 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inosystem.pdv.model;
+
+/**
+ *
+ * @author jrpbj
+ */
+public class RelatorioProduto {
+
+    private String cidade;
+    private String estado;
+    private String uf;
+    private String vendasData;
+    private Integer codigoProduto;
+    private Integer quantidadeProduto;
+    private String nomeProduto;
+    private String unidadeMedida;
+
+    public RelatorioProduto() {
+    }
+
+    /**
+     * @return the cidade
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * @param cidade the cidade to set
+     */
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    /**
+     * @return the estado
+     */
+    public String getEstado() {
+        return estado;
+    }
+
+    /**
+     * @param estado the estado to set
+     */
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    /**
+     * @return the vendasData
+     */
+    public String getVendasData() {
+        return vendasData;
+    }
+
+    /**
+     * @param vendasData the vendasData to set
+     */
+    public void setVendasData(String vendasData) {
+        this.vendasData = vendasData;
+    }
+
+    /**
+     * @return the codigoProduto
+     */
+    public Integer getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    /**
+     * @param codigoProduto the codigoProduto to set
+     */
+    public void setCodigoProduto(Integer codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    /**
+     * @return the quantidadeProduto
+     */
+    public Integer getQuantidadeProduto() {
+        return quantidadeProduto;
+    }
+
+    /**
+     * @param quantidadeProduto the quantidadeProduto to set
+     */
+    public void setQuantidadeProduto(Integer quantidadeProduto) {
+        this.quantidadeProduto = quantidadeProduto;
+    }
+
+    /**
+     * @return the nomeProduto
+     */
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    /**
+     * @param nomeProduto the nomeProduto to set
+     */
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    /**
+     * @return the unidadeMedida
+     */
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    /**
+     * @param unidadeMedida the unidadeMedida to set
+     */
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    /**
+     * @return the uf
+     */
+    public String getUf() {
+        return uf;
+    }
+
+    /**
+     * @param uf the uf to set
+     */
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+}
