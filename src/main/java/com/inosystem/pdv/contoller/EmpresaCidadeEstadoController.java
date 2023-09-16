99e61d9d54e4491d8a6ed840166a7420b52e2044
Jrpbjr <jrpbjr@gmail.com>
2023-09-16 00:01:42 -0300

    add model controller Empresa Cidade Estado

---
+++ src/main/java/com/inosystem/pdv/contoller/EmpresaCidadeEstadoController.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inosystem.pdv.contoller;
+
+import com.inosystem.pdv.dao.EmpresaCidadeEstadoDao;
+import com.inosystem.pdv.model.EmpresaCidadeEstado;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jrpbj
+ */
+public class EmpresaCidadeEstadoController {
+    
+private final EmpresaCidadeEstadoDao daoEmpresaCidadeEstado = new EmpresaCidadeEstadoDao();
+
+    /**
+    * recupera EmpresaCidadeEstado
+    * @param pModelCidade
+    * return EmpresaCidadeEstadoDao
+    */
+    public EmpresaCidadeEstado getEmpresaCidadeEstadoController(int pCodigo){
+        return this.daoEmpresaCidadeEstado.getEmpresaCidadeEstadoDAO(pCodigo);
+        
+        
+    }
+
+    /**
+    * recupera uma lista deEmpresaCidadeEstado
+    * @param pModelCidade
+    * return ArrayList
+    */
+    public ArrayList<EmpresaCidadeEstado> getListaEmpresaCidadeEstadoController(){
+        return this.daoEmpresaCidadeEstado.getListaEmpresaCidadeEstadoDAO();
+    }
+}
