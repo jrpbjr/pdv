ff8de9742c2ce5cbf389fae17170c3160a32f141
Jrpbjr <jrpbjr@gmail.com>
2023-09-18 12:08:30 -0300

    add model controller transportadora cidade estado

---
+++ src/main/java/com/inosystem/pdv/contoller/TransportadoraCidEstController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inosystem.pdv.contoller;
+
+import com.inosystem.pdv.dao.TransportadoraCidEstDao;
+import com.inosystem.pdv.model.TransportadoraCidEst;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jrpbj
+ */
+public class TransportadoraCidEstController {
+    
+private TransportadoraCidEstDao daoTransportadoraCidEst = new TransportadoraCidEstDao();
+
+    /**
+     * recupera uma lista deTransportadoraCidEst
+     *
+     * @param pModelCidade return ArrayList
+     */
+    public ArrayList<TransportadoraCidEst> getListaTransportadoraCidadeEstadoController() {
+        return this.daoTransportadoraCidEst.getListaTransportadoraCidadeEstadoDAO();
+    }
+}
+
