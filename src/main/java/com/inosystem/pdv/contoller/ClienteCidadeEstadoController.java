43ef39fe78e3666dad7ae8f028388e4b237cae25
Jrpbjr <jrpbjr@gmail.com>
2023-09-18 19:55:12 -0300

    add model controller cliente cidade estado

---
+++ src/main/java/com/inosystem/pdv/contoller/ClienteCidadeEstadoController.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inosystem.pdv.contoller;
+
+import com.inosystem.pdv.dao.ClienteCidadeEstadoDao;
+import com.inosystem.pdv.model.ClienteCidadeEstado;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jrpbj
+ */
+public class ClienteCidadeEstadoController {
+    
+private ClienteCidadeEstadoDao dAOClienteCidadeEstado = new ClienteCidadeEstadoDao();
+
+    /**
+    * recupera uma lista deCidadeEstado
+    * @param pModelCidade
+    * return ArrayList
+    */
+    public ArrayList<ClienteCidadeEstado> getListaClienteCidadeEstadoController(){
+        return this.dAOClienteCidadeEstado.getListaClienteCidadeEstadoDAO();
+    }
+
+    public ClienteCidadeEstado getClienteCidadeEstadoController(String pNome) {
+        return this.dAOClienteCidadeEstado.getClienteCidadeEstadoDAO(pNome);
+    }
+    
+    public ClienteCidadeEstado getClienteCidadeEstadoController(int pCodigo) {
+        return this.dAOClienteCidadeEstado.getClienteCidadeEstadoDAO(pCodigo);
+    }
+
+    public ClienteCidadeEstado getClienteCidadeEstadoPorTelefoneController(String pTelefone) {
+        return this.dAOClienteCidadeEstado.getClienteCidadeEstadoPorTelefoneDAO(pTelefone);
+    }
+}
