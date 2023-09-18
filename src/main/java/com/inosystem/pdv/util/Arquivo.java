782d36722654234ef4008ff75a91d0bcf42f01fa
Jrpbjr <jrpbjr@gmail.com>
2023-09-18 18:15:22 -0300

    add manager archive copy

---
+++ src/main/java/com/inosystem/pdv/util/Arquivo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inosystem.pdv.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+/**
+ *
+ * @author jrpbj
+ */
+public class Arquivo {
+    
+public static void copiarArquivos(File source, File destination) throws IOException {
+        if (destination.exists()) {
+            destination.delete();
+        }
+        FileChannel sourceChannel = null;
+        FileChannel destinationChannel = null;
+        try {
+            sourceChannel = new FileInputStream(source).getChannel();
+            destinationChannel = new FileOutputStream(destination).getChannel();
+            sourceChannel.transferTo(0, sourceChannel.size(),
+                    destinationChannel);
+        } finally {
+            if (sourceChannel != null && sourceChannel.isOpen()) {
+                sourceChannel.close();
+            }
+            if (destinationChannel != null && destinationChannel.isOpen()) {
+                destinationChannel.close();
+            }
+        }
+    }
+
+}
+
